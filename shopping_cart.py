3deeb709a496ee75ae4679e62441e67042d9d417
sshtayn <ss13523@stern.nyu.edu>
2021-06-10 22:24:29 -0400

    accounting for matching product/product id and "done"

--- shopping_cart.py
+++ shopping_cart.py
@@ -41,9 +41,15 @@ def to_usd(my_price):
 
 #print(products)
 
-selected_id=input("Please input a product identifier:") 
-matching_products=[p for p in products if str(p["id"])==str(selected_id)]
-matching_product= matching_products[0]
-print("CHOSEN PRODUCT:" + matching_product["name"]+ " "+str(matching_product["price"]))
+while True:
+    
+    selected_id=input("Please input a product identifier:") 
+    if selected_id== "done":
+        break
+    else:
+        matching_products=[p for p in products if str(p["id"])==str(selected_id)]
+        matching_product= matching_products[0]
+        print("CHOSEN PRODUCT:" + matching_product["name"]+ " "+str(matching_product["price"]))
+    
 
 

c98e52ba9296b42a90d8478cf6c039a04990bc73
sshtayn <ss13523@stern.nyu.edu>
2021-06-12 16:01:37 -0400

    converted selected product prices to USD

--- shopping_cart.py
+++ shopping_cart.py
@@ -43,6 +43,7 @@ def to_usd(my_price):
 
 
 import datetime
+import operator
 total_price= 0
 selected_ids=[]
 
@@ -67,12 +68,12 @@ print("--------------------------------------")
 
 print("SELECTED PRODUCTS:")
 for selected_id in selected_ids:
-    matching_products=[p for p in products if str(p["id"])==str(selected_id)]
+    matching_products=[p for p in products if int(p["id"])==int(selected_id)]
     matching_product= matching_products[0]
     total_price= total_price+ (matching_product["price"])
-    print("..." + matching_product["name"]+ " "+str(matching_product["price"]))
+    print("..." + matching_product["name"]+ " "+ "("+to_usd(float(matching_product["price"]))+")")
 
-
+print("--------------------------------------")
 #print(selected_ids)
 print("SUBTOTAL:",to_usd(total_price))
 total_tax=total_price*.0875
